7fb6b81d54e12bf5bbebb8ef31352431f7b85145
vbvsone <vbvsoneprojects@gmail.com>
2026-01-23 21:15:04 -0300

    fix: Grant admin based on nicknames (alvaro, vbvsone)

--- main.py
+++ main.py
@@ -275,7 +275,7 @@ def init_db():
     );
 
     UPDATE usuarios SET es_premium = true;
-    UPDATE usuarios SET es_admin = true WHERE id_usuario = 1;
+    UPDATE usuarios SET es_admin = true WHERE nickname IN ('alvaro', 'vbvsone');
     """
     cur.execute(script_sql)
     conn.commit()
